3aad9db1328731faf9a5c52e9ffe4e9e9c89f6ac
arnonr <arnonr@kmutnb.ac.th>
2023-08-09 17:52:29 +0700

    ssr1

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  ssr: true,
   runtimeConfig: {
     // The private keys which are only available within server-side
     apiSecret: "",
